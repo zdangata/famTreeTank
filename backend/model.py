d7f2e4de3c80d9d42baec2f8c17429f55dce6d74
zdangata <zdangata@gmail.com>
2021-01-12 21:46:27 +0000

    adding gitignore to hide private files

--- backend/model.py
+++ backend/model.py
@@ -8,3 +8,18 @@ import os
 app = Flask(__name__)
 database_path = os.environ.get('SQLALCHEMY_DATABASE_URI')
 db = SQLAlchemy()
+
+#----------------------------------------------------------------------------#
+# Database Setup
+#----------------------------------------------------------------------------#
+
+def setup_db(app, database_path=database_path):
+    app.config["SQLALCHEMY_DATABASE_URI"] = database_path
+    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+    db.app = app
+    db.init_app(app)
+    db.create_all()
+
+#----------------------------------------------------------------------------#
+# Models.
+#----------------------------------------------------------------------------#
